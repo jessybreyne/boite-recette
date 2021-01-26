30e7ad81513920a04e161895534e671358b771d2
Jessy <jessybreyne@protonmail.com>
2021-01-26 16:54:11 +0100

    Connexion à la base de données

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ import Card from './components/Card'
 import Header from './components/Header'
 import recettes from './recettes'
 
+// firebase
+import base from './base'
+
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
     recettes: {}
+  }
+
+  componentDidMount () {
+    base.syncState(`/${this.state.pseudo}/recettes`, {
+      context: this,
+      state: 'recettes'
+    })
   }
 
   chargerExemple = () => {
@@ -19,7 +29,7 @@ class App extends Component {
   render () {
     const cards = Object.keys(this.state.recettes)
       .map(key => <Card key={key} details={this.state.recettes[key]}/>)
-      
+
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo} />
--- src/base.js
+++ src/base.js
@@ -3,10 +3,23 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: "AIzaSyBbATC07N4FLLBmOIQbCksuI1jqZCxJ8TU",
+  authDomain: "recette-app-fe16b.firebaseapp.com",
+  databaseURL: "https://recette-app-fe16b-default-rtdb.firebaseio.com"
 })
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBbATC07N4FLLBmOIQbCksuI1jqZCxJ8TU",
+  authDomain: "recette-app-fe16b.firebaseapp.com",
+  databaseURL: "https://recette-app-fe16b-default-rtdb.firebaseio.com",
+  projectId: "recette-app-fe16b",
+  storageBucket: "recette-app-fe16b.appspot.com",
+  messagingSenderId: "962205627565",
+  appId: "1:962205627565:web:6dd6835b5f5d33e8234224"
+};
+
+
 
 const base = Rebase.createClass(firebaseApp.database())
 
