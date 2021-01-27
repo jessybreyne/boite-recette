7b163b532bc6151a84ba8294e944036218a11230
Jessy <jessybreyne@protonmail.com>
2021-01-27 10:24:20 +0100

    Remove one problem

--- src/base.js
+++ src/base.js
@@ -8,19 +8,6 @@ const firebaseApp = firebase.initializeApp({
   databaseURL: "https://recette-app-fe16b-default-rtdb.firebaseio.com"
 })
 
-
-var firebaseConfig = {
-  apiKey: "AIzaSyBbATC07N4FLLBmOIQbCksuI1jqZCxJ8TU",
-  authDomain: "recette-app-fe16b.firebaseapp.com",
-  databaseURL: "https://recette-app-fe16b-default-rtdb.firebaseio.com",
-  projectId: "recette-app-fe16b",
-  storageBucket: "recette-app-fe16b.appspot.com",
-  messagingSenderId: "962205627565",
-  appId: "1:962205627565:web:6dd6835b5f5d33e8234224"
-};
-
-
-
 const base = Rebase.createClass(firebaseApp.database())
 
 // This is a named export
